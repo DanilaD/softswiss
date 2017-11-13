02b767f3db3bf0def305e47b437e396cbe650761
DanilaD <danila@autosport.by>
2017-11-13 09:27:26 +0300

    add function addMoney

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,32 @@ class UserController extends Controller
         
     }
     
+        
+    /**
+     * Add money to user id.
+     *
+     * @param  int  $id int $amount
+     * @return Response
+     */
+    public function addMoney(Request $request)
+    {
+      
+        $validator = Validator::make($request->all(), [
+            'user' => 'required|integer',
+            'amount' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) 
+        {
+            return response($validator->errors(), 422);
+        }
+  
+        if (User::updateOrCreate(
+              ['id' => $request->input('user')])
+              ->increment('balance', $request->input('amount'), ['id' => $request->input('user')]))              
+        {
+            return response('', 200);
+        }        
+    }
+    
 }
